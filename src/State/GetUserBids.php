da325b5a47ba6efd3bbddaa690a785da767e1dae
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-03 16:21:13 -0500

    If client pagination is sent we'll use it else we'll use defaults

--- src/State/GetUserBids.php
+++ src/State/GetUserBids.php
@@ -12,9 +12,11 @@ class GetUserBids implements ProviderInterface
     public function __construct(private readonly EntityManagerInterface $manager)
     {
     }
-    public function provide(Operation $operation, array $uriVariables = [], array $context = []): array
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
         $userId = $uriVariables["id"];
-        return $this->manager->getRepository(Bid::class)->getUserBids($userId);
+        $itemsPerPage = (int)$context["filters"]["itemsPerPage"] ?: 30;
+        $page = (int)$context["filters"]["page"] ?: 1;
+        return $this->manager->getRepository(Bid::class)->getUserBids($userId, $page, $itemsPerPage);
     }
 }
