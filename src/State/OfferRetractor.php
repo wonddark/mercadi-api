77beaa5eda3d3b0ffae01c391aefc3a8928d879e
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-06-22 21:24:46 -0400

    State processor for offer action delete

---
+++ src/State/OfferRetractor.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Offer;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+
+class OfferRetractor implements ProcessorInterface
+{
+    public function __construct(
+        private readonly ProcessorInterface     $processor,
+        private readonly EntityManagerInterface $entityManager
+    )
+    {
+    }
+
+    public function process(
+        /* @var Offer $data */
+        mixed     $data,
+        Operation $operation,
+        array     $uriVariables = [],
+        array     $context = []
+    ): void
+    {
+        if ($data->isDeletable()) {
+            $bids = $data->getBids();
+
+            if ($bids->getHighestOffer() === $data->getQuantity()) {
+                $index = $bids->getOffers()->indexOf($data);
+                $previousOffer = $bids->getOffers()->toArray()[$index - 1];
+                $bids->setHighestOffer($previousOffer->getQuantity());
+                $this->entityManager->persist($bids);
+            }
+            $this->processor->process($data, $operation, $uriVariables, $context);
+        } else {
+            throw new UnprocessableEntityHttpException("Offer is not deletable");
+        }
+    }
+}
